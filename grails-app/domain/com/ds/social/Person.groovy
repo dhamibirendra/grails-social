b5a86fe1b7fd72f4056c8c9621dc85055b387357
birendra <dhamibirendra@gmail.com>
2014-04-24 11:44:44 +0545

    project startup

---
+++ grails-app/domain/com/ds/social/Person.groovy
@@ -0,0 +1,44 @@
+package com.ds.social
+
+class Person {
+
+	transient springSecurityService
+
+	String username
+	String password
+	boolean enabled = true
+	boolean accountExpired
+	boolean accountLocked
+	boolean passwordExpired
+
+    static hasMany = [oAuthIDs: OAuthID]
+
+	static transients = ['springSecurityService']
+
+	static constraints = {
+		username blank: false, unique: true
+		password blank: false
+	}
+
+	static mapping = {
+		password column: '`password`'
+	}
+
+	Set<Authority> getAuthorities() {
+		PersonAuthority.findAllByPerson(this).collect { it.authority } as Set
+	}
+
+	def beforeInsert() {
+		encodePassword()
+	}
+
+	def beforeUpdate() {
+		if (isDirty('password')) {
+			encodePassword()
+		}
+	}
+
+	protected void encodePassword() {
+		password = springSecurityService.encodePassword(password)
+	}
+}
